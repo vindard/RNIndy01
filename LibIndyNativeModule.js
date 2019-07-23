74d552397f14b36f57ec63a295cb228d217cb52d
User <17693119+vindard@users.noreply.github.com>
2019-07-23 09:11:59 -0400

    add more console.log msgs to JS frontend

--- LibIndyNativeModule.js
+++ LibIndyNativeModule.js
@@ -8,10 +8,13 @@ export default {
   async exampleMethod () {
     console.log("Ran JS wrapper!");
     await LibIndy.init();
+    console.log("Loaded Libindy library (LibIndy.init)");
     const text = await LibIndy.exampleMethod();
-    console.log(text);
+    console.log("Ran example method from Java:", text);
+    console.log("Running async Libindy.createWallet() method...");
     const wallet = await LibIndy.createWallet();
-    console.log(wallet)
+    console.log("Wallet handle: ", wallet);
+    console.log("Ran createWallet()!");
   },
 
   EXAMPLE_CONSTANT: LibIndy.EXAMPLE_CONSTANT
